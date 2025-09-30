9749f9826ffc6cdf7e26af6039d1120d1cf20eec
moshito <mosho@me.com>
2025-09-30 19:53:02 +0200

    Added loop for every element and started to add HTML elements

--- pokecard.js
+++ pokecard.js
@@ -1,27 +1,38 @@
 /*
  * @Author: MoS 
  * @Date: 2025-09-30 13:05:51 
- * @Last Modified by: MoS
- * @Last Modified time: 2025-09-30 18:19:04
+ * @Last Modified by: Mos
+ * @Last Modified time: 2025-09-30 19:52:09
  */
-
-//const pokemonCardList = document.getElementById("pokemonCardList")
+console.log("Powered by: MosKaffe\n===========")
+const pokemonCard = document.getElementsByClassName("pokemonCard")[0]
 
 async function getPokemontList() {
-const getPokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-const returnPokemons = await getPokemons.json()
+    const getPokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    const returnPokemons = await getPokemons.json()
+        
+        for (let pokemon of returnPokemons.results) {
 
-const getOnePokeData = await fetch(returnPokemons.results[0].url)
-const returnOnePokeData = await getOnePokeData.json()
+        const getOnePokeData = await fetch(pokemon.url)
+        const returnOnePokeData = await getOnePokeData.json()
+        
+        const getSpeData = await fetch(returnOnePokeData.species.url)
+        const returnSpeData = await getSpeData.json()
+        
+        const getEvoData = await fetch(returnSpeData.evolution_chain.url)
+        const returnEvoData = await getEvoData.json()
+        
+        //Testar output i consolen
+        //console.log(returnOnePokeData.sprites.back_default)
+        //console.log(returnOnePokeData.name, returnOnePokeData.sprites.back_default);
+        
+        const pokeCardHolder = document.createElement("div")
+        pokeCardHolder.classList.add("pokemonCard")
 
-const getSpeData = await fetch(returnOnePokeData.species.url)
-const returnSpeData = await getSpeData.json()
-
-const getEvoData = await fetch(returnSpeData.evolution_chain.url)
-const returnEvoData = await getEvoData.json()
-
-console.log(returnOnePokeData.name,returnOnePokeData.base_experience,returnSpeData.color.name, returnEvoData.baby_trigger_item)
-
+        const pokeCardImg = document.createElement("img")
+        pokeCardImg.src = returnOnePokeData.sprites.back_default
+        pokeCardImg.alt = pokemon.name
+    }
 }
 
 getPokemontList()
